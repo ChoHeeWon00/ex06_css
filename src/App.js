2e9bc33ebb35d3bd60025c446f91d6a4822a811b
heewon <heewon@won.com>
2025-04-07 15:16:27 +0900

    ex06_css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import TestCom01 from './components/TestCom01';
+import TestCom02 from './components/TestCom02';
+import TestCom03 from './components/TestCom03';
+import { Route, Routes } from 'react-router-dom';
+import IndexCon from './containers/IndexCon';
+import LoginCon from './containers/LoginCon';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<>
+  <Routes>
+    <Route path="/" element={<IndexCon />} />
+    <Route path="/login" element={<LoginCon />} />
+  </Routes>
+  {/*
+    <TestCom03 name="홍길동" />
+    <hr />
+    <TestCom02 />
+    <hr />
+    <TestCom01 />
+  */}
+    </>);
 }
 
 export default App;
---
+++ src/containers/LoginCon.js
@@ -0,0 +1,9 @@
+import HeaderCom from "../components/common/HeaderCom";
+import LoginCom from "../components/LoginCom";
+function LoginCon() {
+    return (<>
+        <HeaderCom />
+        <LoginCom />
+      </>);
+  }
+  export default LoginCon;
